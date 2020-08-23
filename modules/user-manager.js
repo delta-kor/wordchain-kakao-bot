006746a8ef815f185b15169916c07bc6f0f32724
delta-kor <deltakbot1@gmail.com>
2020-08-23 18:07:01 +0900

    Add user manager methods

--- modules/user-manager.js
+++ modules/user-manager.js
@@ -9,6 +9,26 @@ module.exports = (function() {
 
     function UserManager() { }
 
+    UserManager.has = function(id) {
+        return !!users.get(id);
+    };
+
+    UserManager.get = function(id) {
+        return users.get(id);
+    };
+
+    UserManager.find = function(nickname, avatar, channel) {
+        for(let user of users.values()) {
+            if(user.nickname === nickname, user.avatar === avatar && user.channel === channel)
+                return user;
+        }
+        return null;
+    };
+
+    UserManager.push = function(user) {
+        users.set(user.id, user);
+    };
+
     return UserManager;
 
 })();
